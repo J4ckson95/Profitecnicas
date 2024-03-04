5035553e23c124633c1bd50bb6b4a8be4afc3369
Jason López <j4ckson95@gmail.com>
2024-03-04 07:49:34 -0500

    Adelanto Header, terminado top

--- src/components/Home/Header/Head/Head.jsx
+++ src/components/Home/Header/Head/Head.jsx
@@ -1,6 +1,21 @@
+import titleLogo from "../../../../asset/logo.png"
+import cart from "../../../../asset/carrito-de-compras.png"
+import style from "./head.module.css"
 const Head = () => {
     return (
-        <div>HEAD</div>
+        <div className={style.Head}>
+            <div>
+                <img src={titleLogo}></img>
+            </div>
+            <div className={style.Head_Search}>
+                <input placeholder="Titulo, Autor o ISBN"></input>
+                <button></button>
+            </div>
+            <div className={style.Head_Count}>
+                <img src={cart}></img>
+                <p>Mi Compra</p>
+            </div>
+        </div>
     );
 }
 export default Head;
--- src/components/Home/Header/Top/Top.jsx
+++ src/components/Home/Header/Top/Top.jsx
@@ -2,10 +2,10 @@ import styled from "./top.module.css"
 const Top = () => {
     return (
         <div className={styled.top_container}>
-            <ul>
-                <li>Quines Somos</li>
-                <li>Contacto</li>
-                <li>Mi Cuenta</li>
+            <ul className={styled.container}>
+                <li><a>Quines Somos</a></li>
+                <li><a>Contacto</a></li>
+                <li className={styled.count}><a>Mi Cuenta</a></li>
             </ul>
         </div>
     );
