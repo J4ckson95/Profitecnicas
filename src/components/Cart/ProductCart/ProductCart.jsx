2109d8ff6c604940b7ce5394d65eb00f9b793da5
Jason López <j4ckson95@gmail.com>
2024-05-05 20:11:05 -0500

    Carrito de compras finalizado, pendiente pasarela de pago

--- src/components/Cart/ProductCart/ProductCart.jsx
+++ src/components/Cart/ProductCart/ProductCart.jsx
@@ -1,41 +1,25 @@
-import { useEffect, useState } from "react";
-import style from "./productcart.module.css"
-const ProductCart = ({ data, onPrice, ResetTotal }) => {
-    const [product, setProduct] = useState([])
-    const [total, setTotal] = useState(0)
-    const [quantity, setQuantity] = useState(data.quantity)
-    useEffect(() => {
-        const getData = async () => {
-            const query = await fetch(`http://localhost:8080/api/products/${data._id}`, { credentials: "include" })
-            const result = await query.json()
-            const total = data.quantity * result.payload.price
-            onPrice(total);
-            setProduct(result.payload)
-            setTotal(total)
-        }
-        getData()
-    }, []);
-    const handleLess = () => {
-        if (quantity > 0) {
-            setQuantity(prevQuantity => prevQuantity - 1);
-            const newTotal = product.price * (quantity - 1);
-            setTotal(newTotal);
-            onPrice(newTotal);
-        }
-    };
 
-    const handleMore = () => {
-        setQuantity(prevQuantity => prevQuantity + 1);
-        const newTotal = product.price * (quantity + 1);
-        setTotal(newTotal);
-        onPrice(newTotal);
-    };
+import style from "./productcart.module.css"
+const ProductCart = ({ data, setChange }) => {
+    const totalPrice = data.quantity * data.product.price
+    const updateCart = (operation) => {
+        let newQuantity = data.quantity
+        if (operation === "more") newQuantity = newQuantity + 1
+        if (operation === "less") newQuantity = newQuantity - 1
+        fetch(`http://localhost:8080/api/carts/updatecart/${data.product._id}/${newQuantity}`,
+            {
+                credentials: "include",
+                method: "PATCH"
+            }).then((res) => {
+                if (res.status === 200) setChange(true)
+            })
+    }
     return (
         <tr>
-            <td className={style.article}><h3>{product.title}</h3><img src={product.thumbnail}></img></td>
-            <td className={style.quantity}><button onClick={handleLess}>-</button><h3>{quantity}</h3><button onClick={handleMore}>+</button></td>
-            <td><h3>{product.price}</h3></td>
-            <td><h3>{total}</h3></td>
+            <td className={style.article}><h3>{data.product.title}</h3><img src={data.product.thumbnail}></img></td>
+            <td className={style.quantity}><button onClick={() => updateCart("less")}>-</button><h3>{data.quantity}</h3><button onClick={() => updateCart("more")}>+</button></td>
+            <td><h3>{data.product.price}</h3></td>
+            <td><h3>{totalPrice}</h3></td>
         </tr>
     );
 }
