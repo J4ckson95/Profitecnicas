7e63fd99aefd7aa0c20e9c6fcca90126cfb3e0b6
Jason López <j4ckson95@gmail.com>
2024-05-28 07:35:24 -0500

    implementación stripe

--- src/components/Payment/Payment.jsx
+++ src/components/Payment/Payment.jsx
@@ -7,16 +7,23 @@ const Payment = () => {
     const [cart, setCart] = useState([])
     const [clienteSecret, setClienteSecret] = useState("")
     useEffect(() => {
-        fetch(`http://localhost:8080/api/carts/`, { credentials: "include" })
-            .then((res) => res.json())
-            .then((data) => setCart(data))
-        fetch("http://localhost:8080/api/payment/create-payment-intent", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(cart) //! Pendiente Items del carrito 
-        })
-            .then((res) => res.json())
-            .then((data) => setClienteSecret(data.clientesecret))
+        const getCart = () => {
+            fetch(`http://localhost:8080/api/carts/`, { credentials: "include" })
+                .then((res) => res.json())
+                .then((data) => setCart(data))
+        }
+        const payment = () => {
+            fetch("http://localhost:8080/api/payment/create-payment-intent", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(cart) //! Pendiente Items del carrito 
+            })
+                .then((res) => res.json())
+                .then((data) => setClienteSecret(data.clientesecret))
+        }
+        getCart()
+        console.log(cart);
+        payment()
     }, []);
     const appearance = {
         theme: "stripe"
